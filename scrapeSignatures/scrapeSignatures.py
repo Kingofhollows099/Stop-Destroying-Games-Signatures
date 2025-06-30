96f77acaf5139c0ab3f169af1f765956ee5e51e1
Kingofhollows099 <kingofhollows099@gmail.com>
2025-06-30 04:37:02 -0500

    Auto update: 30-Mon-06_04-37

--- scrapeSignatures/scrapeSignatures.py
+++ scrapeSignatures/scrapeSignatures.py
@@ -34,16 +34,6 @@ def scrape_signatures():
     except Exception as e:
         print(f"An unexpected error occurred: {e}")
 
-def updateExcel():
-    excel_path = r"C:\Users\Geno\OneDrive\Documents\Signatures over time.xlsx"
-    csv_path = r"C:\Users\Geno\Documents\GitHub\Stop-Destroying-Games-Signatures\scrapeSignatures\signatures.csv"
-
-    df_csv = pd.read_csv(csv_path)
-
-    with pd.ExcelWriter(excel_path, engine='openpyxl', mode='a', if_sheet_exists='replace') as writer:
-        df_csv.to_excel(writer, sheet_name='Signature data', index=False)
-
-
 if __name__ == "__main__":
     scrape_signatures()
 
