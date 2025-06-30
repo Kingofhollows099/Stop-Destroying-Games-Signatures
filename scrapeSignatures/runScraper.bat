29541061f615a00b95d02d1bad25d8b803249f11
Kingofhollows099 <kingofhollows099@gmail.com>
2025-06-30 03:59:32 -0500

    Auto update: 30-Mon-06_03-59

--- scrapeSignatures/runScraper.bat
+++ scrapeSignatures/runScraper.bat
@@ -3,7 +3,7 @@ echo "Phase 1 starting"
 python3 scrapeSignatures.py
 echo Last Run: %date% %time% > lastRun.txt
 echo "Phase 2 starting"
-call pushToGit.batecho "Phease 1 starting"
+call pushToGit.bat
 echo "Phase 3 starting"
 pause
 powershell.exe -NoProfile -ExecutionPolicy Bypass -WindowStyle Hidden -NonInteractive -File "%~dp0Refresh signatures.ps1"
