15e2ccb829ff94acae009a466baff1731092634b
Kingofhollows099 <kingofhollows099@gmail.com>
2025-06-30 03:44:20 -0500

    Auto update: 30-Mon-06_03-44

---
+++ scrapeSignatures/Refresh signatures.ps1
@@ -0,0 +1,11 @@
+$excelFilePath = "C:\Users\Geno\OneDrive\Documents\Signatures over time.xlsx"
+$excelApp = New-Object -ComObject "Excel.Application"
+$excelApp.Visible = $false
+$workbook = $excelApp.Workbooks.Open($excelFilePath)
+$workbook.RefreshAll()
+Start-Sleep -Seconds 30  # Wait for refresh to complete
+$workbook.Save()
+$workbook.Close()
+$excelApp.Quit()
+Remove-Variable excelApp, workbook
+exit
--- scrapeSignatures/runScraper.bat
+++ scrapeSignatures/runScraper.bat
@@ -2,3 +2,4 @@
 python3 scrapeSignatures.py
 echo Last Run: %date% %time% > lastRun.txt
 call pushToGit.bat
+powershell.exe -NoProfile -ExecutionPolicy Bypass -WindowStyle Hidden -NonInteractive -File "%~dp0Refresh signatures.ps1"
